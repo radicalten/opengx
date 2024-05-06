90c6e36fec508ac9745a25ce96cac2322838523b
Alberto Mardegan <mardy@users.sourceforge.net>
2024-05-06 18:58:45 +0300

    Run clang-format on image_DTX files

--- src/image_DXT.h
+++ src/image_DXT.h
@@ -1,18 +1,17 @@
 /*
-	Jonathan Dummer
-	2007-07-31-10.32
+        Jonathan Dummer
+        2007-07-31-10.32
 
-	simple DXT compression / decompression code
+        simple DXT compression / decompression code
 
-	public domain
+        public domain
 */
 
-#ifndef HEADER_IMAGE_DXT
-#define HEADER_IMAGE_DXT
+#ifndef OPENGX_IMAGE_DXT_H
+#define OPENGX_IMAGE_DXT_H
 
-void _ogx_convert_rgb_image_to_DXT1 (
-		const unsigned char *const uncompressed, unsigned char *compressed, 
-		int width, int height, int red_blue_swap
-);
+void _ogx_convert_rgb_image_to_DXT1(
+    const unsigned char *const uncompressed, unsigned char *compressed,
+    int width, int height, int red_blue_swap);
 
-#endif /* HEADER_IMAGE_DXT	*/
+#endif /* OPENGX_IMAGE_DXT_H */
