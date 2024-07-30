dbe68ae165c3d2efd0b14f67454334a6168e841f
Alberto Mardegan <mardy@users.sourceforge.net>
2024-07-30 22:21:57 +0300

    Add all missing glVertex, glColor, glNormal, glTexCoord functions

--- src/vertex.cpp
+++ src/vertex.cpp
@@ -36,6 +36,57 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include <GL/gl.h>
 #include <malloc.h>
+#include <type_traits>
+
+template <typename T>
+T full_color()
+{
+    if constexpr (std::is_floating_point<T>::value) {
+        return 1.0f;
+    } else {
+        return std::numeric_limits<T>::max();
+    }
+}
+
+template <typename T>
+void set_current_color(T red, T green, T blue, T alpha = full_color<T>())
+{
+    if (glparamstate.imm_mode.in_gl_begin)
+        glparamstate.imm_mode.has_color = 1;
+
+    auto &c = glparamstate.imm_mode.current_color;
+    if constexpr (std::is_floating_point<T>::value) {
+        c[0] = red;
+        c[1] = green;
+        c[2] = blue;
+        c[3] = alpha;
+    } else {
+        constexpr float max = std::numeric_limits<T>::max();
+        /* The OpenGL specification says that for signed components the most
+         * negative value representable by T should be mapped to -1.0, whereas
+         * with our conversion its getting converted to a bit less than that,
+         * but let's fix this only if it turns out to be a real issue. */
+        c[0] = red / max;
+        c[1] = green / max;
+        c[2] = blue / max;
+        c[3] = alpha / max;
+    }
+}
+
+static inline void set_current_tex_coords(float s, float t = 0)
+{
+    auto &c = glparamstate.imm_mode.current_texcoord;
+    c[0] = s;
+    c[1] = t;
+}
+
+static inline void set_current_tex_coords(float s, float t, float r, float q = 1.0f)
+{
+    set_current_tex_coords(s, t);
+    if (r != 0.0f || q != 1.0f) {
+        warning("glTexCoord{3,4}* not supported");
+    }
+}
 
 void glVertex2d(GLdouble x, GLdouble y)
 {
@@ -50,6 +101,16 @@ void glVertex2f(GLfloat x, GLfloat y)
 void glVertex2i(GLint x, GLint y)
 {
     glVertex3f(x, y, 0.0f);
+}
+
+void glVertex2s(GLshort x, GLshort y)
+{
+    glVertex2f(x, y);
+}
+
+void glVertex3d(GLdouble x, GLdouble y, GLdouble z)
+{
+    glVertex3f(x, y, z);
 }
 
 void glVertex3f(GLfloat x, GLfloat y, GLfloat z)
@@ -82,9 +143,39 @@ void glVertex3f(GLfloat x, GLfloat y, GLfloat z)
     vert->pos[2] = z;
 }
 
+void glVertex3i(GLint x, GLint y, GLint z)
+{
+    glVertex3f(x, y, z);
+}
+
+void glVertex3s(GLshort x, GLshort y, GLshort z)
+{
+    glVertex3f(x, y, z);
+}
+
+void glVertex4d(GLdouble x, GLdouble y, GLdouble z, GLdouble w)
+{
+    glVertex4f(x, y, z, w);
+}
+
 void glVertex4f(GLfloat x, GLfloat y, GLfloat z, GLfloat w)
 {
     glVertex3f(x / w, y / w, z / w);
+}
+
+void glVertex4i(GLint x, GLint y, GLint z, GLint w)
+{
+    glVertex4f(x, y, z, w);
+}
+
+void glVertex4s(GLshort x, GLshort y, GLshort z, GLshort w)
+{
+    glVertex4f(x, y, z, w);
+}
+
+void glVertex2dv(const GLdouble *v)
+{
+    glVertex2d(v[0], v[1]);
 }
 
 void glVertex2fv(const GLfloat *v)
@@ -92,9 +183,64 @@ void glVertex2fv(const GLfloat *v)
     glVertex2f(v[0], v[1]);
 }
 
+void glVertex2iv(const GLint *v)
+{
+    glVertex2i(v[0], v[1]);
+}
+
+void glVertex2sv(const GLshort *v)
+{
+    glVertex2s(v[0], v[1]);
+}
+
+void glVertex3dv(const GLdouble *v)
+{
+    glVertex3d(v[0], v[1], v[2]);
+}
+
 void glVertex3fv(const GLfloat *v)
 {
     glVertex3f(v[0], v[1], v[2]);
+}
+
+void glVertex3iv(const GLint *v)
+{
+    glVertex3i(v[0], v[1], v[2]);
+}
+
+void glVertex3sv(const GLshort *v)
+{
+    glVertex3s(v[0], v[1], v[2]);
+}
+
+void glVertex4dv(const GLdouble *v)
+{
+    glVertex4d(v[0], v[1], v[2], v[3]);
+}
+
+void glVertex4fv(const GLfloat *v)
+{
+    glVertex4f(v[0], v[1], v[2], v[3]);
+}
+
+void glVertex4iv(const GLint *v)
+{
+    glVertex4i(v[0], v[1], v[2], v[3]);
+}
+
+void glVertex4sv(const GLshort *v)
+{
+    glVertex4s(v[0], v[1], v[2], v[3]);
+}
+
+void glNormal3b(GLbyte nx, GLbyte ny, GLbyte nz)
+{
+    glNormal3f(nx, ny, nz);
+}
+
+void glNormal3d(GLdouble nx, GLdouble ny, GLdouble nz)
+{
+    glNormal3f(nx, ny, nz);
 }
 
 void glNormal3f(GLfloat nx, GLfloat ny, GLfloat nz)
@@ -104,93 +250,190 @@ void glNormal3f(GLfloat nx, GLfloat ny, GLfloat nz)
     glparamstate.imm_mode.current_normal[2] = nz;
 }
 
+void glNormal3i(GLint nx, GLint ny, GLint nz)
+{
+    glNormal3f(nx, ny, nz);
+}
+
+void glNormal3s(GLshort nx, GLshort ny, GLshort nz)
+{
+    glNormal3f(nx, ny, nz);
+}
+
+void glNormal3bv(const GLbyte *v)
+{
+    glNormal3b(v[0], v[1], v[2]);
+}
+
+void glNormal3dv(const GLdouble *v)
+{
+    glNormal3d(v[0], v[1], v[2]);
+}
+
 void glNormal3fv(const GLfloat *v)
 {
-    glparamstate.imm_mode.current_normal[0] = v[0];
-    glparamstate.imm_mode.current_normal[1] = v[1];
-    glparamstate.imm_mode.current_normal[2] = v[2];
+    glNormal3f(v[0], v[1], v[2]);
+}
+
+void glNormal3iv(const GLint *v)
+{
+    glNormal3i(v[0], v[1], v[2]);
+}
+
+void glNormal3sv(const GLshort *v)
+{
+    glNormal3s(v[0], v[1], v[2]);
+}
+
+void glColor3b(GLbyte red, GLbyte green, GLbyte blue)
+{
+    set_current_color(red, green, blue);
+}
+
+void glColor3d(GLdouble red, GLdouble green, GLdouble blue)
+{
+    set_current_color(red, green, blue);
 }
 
 void glColor3f(GLfloat red, GLfloat green, GLfloat blue)
 {
-    if (glparamstate.imm_mode.in_gl_begin)
-        glparamstate.imm_mode.has_color = 1;
-    glparamstate.imm_mode.current_color[0] = clampf_01(red);
-    glparamstate.imm_mode.current_color[1] = clampf_01(green);
-    glparamstate.imm_mode.current_color[2] = clampf_01(blue);
-    glparamstate.imm_mode.current_color[3] = 1.0f;
+    set_current_color(red, green, blue);
+}
+
+void glColor3i(GLint red, GLint green, GLint blue)
+{
+    set_current_color(red, green, blue);
+}
+
+void glColor3s(GLshort red, GLshort green, GLshort blue)
+{
+    set_current_color(red, green, blue);
 }
 
 void glColor3ub(GLubyte red, GLubyte green, GLubyte blue)
 {
-    glColor3f(red / 255.0f, green / 255.0f, blue / 255.0f);
+    set_current_color(red, green, blue);
+}
+
+void glColor3ui(GLuint red, GLuint green, GLuint blue)
+{
+    set_current_color(red, green, blue);
+}
+
+void glColor3us(GLushort red, GLushort green, GLushort blue)
+{
+    set_current_color(red, green, blue);
+}
+
+void glColor4b(GLbyte red, GLbyte green, GLbyte blue, GLbyte alpha)
+{
+    set_current_color(red, green, blue, alpha);
+}
+
+void glColor4d(GLdouble red, GLdouble green, GLdouble blue, GLdouble alpha)
+{
+    set_current_color(red, green, blue, alpha);
 }
 
 void glColor4f(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
 {
-    if (glparamstate.imm_mode.in_gl_begin)
-        glparamstate.imm_mode.has_color = 1;
-    glparamstate.imm_mode.current_color[0] = clampf_01(red);
-    glparamstate.imm_mode.current_color[1] = clampf_01(green);
-    glparamstate.imm_mode.current_color[2] = clampf_01(blue);
-    glparamstate.imm_mode.current_color[3] = clampf_01(alpha);
+    set_current_color(red, green, blue, alpha);
 }
 
-void glColor4ub(GLubyte r, GLubyte g, GLubyte b, GLubyte a)
+void glColor4i(GLint red, GLint green, GLint blue, GLint alpha)
 {
-    if (glparamstate.imm_mode.in_gl_begin)
-        glparamstate.imm_mode.has_color = 1;
-    glparamstate.imm_mode.current_color[0] = r / 255.0f;
-    glparamstate.imm_mode.current_color[1] = g / 255.0f;
-    glparamstate.imm_mode.current_color[2] = b / 255.0f;
-    glparamstate.imm_mode.current_color[3] = a / 255.0f;
+    set_current_color(red, green, blue, alpha);
 }
 
-void glColor3fv(const GLfloat *v)
+void glColor4s(GLshort red, GLshort green, GLshort blue, GLshort alpha)
 {
-    glColor3f(v[0], v[1], v[2]);
+    set_current_color(red, green, blue, alpha);
 }
 
-void glColor4fv(const GLfloat *v)
+void glColor4ub(GLubyte red, GLubyte green, GLubyte blue, GLubyte alpha)
 {
-    if (glparamstate.imm_mode.in_gl_begin)
-        glparamstate.imm_mode.has_color = 1;
-    glparamstate.imm_mode.current_color[0] = clampf_01(v[0]);
-    glparamstate.imm_mode.current_color[1] = clampf_01(v[1]);
-    glparamstate.imm_mode.current_color[2] = clampf_01(v[2]);
-    glparamstate.imm_mode.current_color[3] = clampf_01(v[3]);
+    set_current_color(red, green, blue, alpha);
 }
 
-void glColor4ubv(const GLubyte *color)
+void glColor4ui(GLuint red, GLuint green, GLuint blue, GLuint alpha)
 {
-    if (glparamstate.imm_mode.in_gl_begin)
-        glparamstate.imm_mode.has_color = 1;
-    glparamstate.imm_mode.current_color[0] = color[0] / 255.0f;
-    glparamstate.imm_mode.current_color[1] = color[1] / 255.0f;
-    glparamstate.imm_mode.current_color[2] = color[2] / 255.0f;
-    glparamstate.imm_mode.current_color[3] = color[3] / 255.0f;
+    set_current_color(red, green, blue, alpha);
 }
 
-void glTexCoord2d(GLdouble u, GLdouble v)
+void glColor4us(GLushort red, GLushort green, GLushort blue, GLushort alpha)
 {
-    glTexCoord2f(u, v);
+    set_current_color(red, green, blue, alpha);
 }
 
-void glTexCoord2f(GLfloat u, GLfloat v)
+void glColor3bv(const GLbyte *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3dv(const GLdouble *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3fv(const GLfloat *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3iv(const GLint *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3sv(const GLshort *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3ubv(const GLubyte *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3uiv(const GLuint *v) { set_current_color(v[0], v[1], v[2]); }
+void glColor3usv(const GLushort *v) { set_current_color(v[0], v[1], v[2]); }
+
+void glColor4bv(const GLbyte *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4dv(const GLdouble *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4fv(const GLfloat *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4iv(const GLint *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4sv(const GLshort *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4ubv(const GLubyte *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4uiv(const GLuint *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+void glColor4usv(const GLushort *v) { set_current_color(v[0], v[1], v[2], v[3]); }
+
+void glTexCoord1d(GLdouble s) { set_current_tex_coords(s); }
+void glTexCoord1f(GLfloat s) { set_current_tex_coords(s); }
+void glTexCoord1i(GLint s) { set_current_tex_coords(s); }
+void glTexCoord1s(GLshort s) { set_current_tex_coords(s); }
+
+void glTexCoord2d(GLdouble s, GLdouble t) { set_current_tex_coords(s, t); }
+void glTexCoord2f(GLfloat s, GLfloat t) { set_current_tex_coords(s, t); }
+void glTexCoord2i(GLint s, GLint t) { set_current_tex_coords(s, t); }
+void glTexCoord2s(GLshort s, GLshort t) { set_current_tex_coords(s, t); }
+
+void glTexCoord3d(GLdouble s, GLdouble t, GLdouble r) { set_current_tex_coords(s, t, r); }
+void glTexCoord3f(GLfloat s, GLfloat t, GLfloat r) { set_current_tex_coords(s, t, r); }
+void glTexCoord3i(GLint s, GLint t, GLint r) { set_current_tex_coords(s, t, r); }
+void glTexCoord3s(GLshort s, GLshort t, GLshort r) { set_current_tex_coords(s, t, r); }
+
+void glTexCoord4d(GLdouble s, GLdouble t, GLdouble r, GLdouble q)
 {
-    glparamstate.imm_mode.current_texcoord[0] = u;
-    glparamstate.imm_mode.current_texcoord[1] = v;
+    set_current_tex_coords(s, t, r, q);
 }
 
-void glTexCoord2i(GLint s, GLint t)
+void glTexCoord4f(GLfloat s, GLfloat t, GLfloat r, GLfloat q)
 {
-    glTexCoord2f(s, t);
+    set_current_tex_coords(s, t, r, q);
 }
 
-void glTexCoord3f(GLfloat s, GLfloat t, GLfloat r)
+void glTexCoord4i(GLint s, GLint t, GLint r, GLint q)
 {
-    glTexCoord2f(s, t);
-    if (r != 0.0) {
-        warning("glTexCoord3f not supported");
-    }
+    set_current_tex_coords(s, t, r, q);
 }
+
+void glTexCoord4s(GLshort s, GLshort t, GLshort r, GLshort q)
+{
+    set_current_tex_coords(s, t, r, q);
+}
+
+void glTexCoord1dv(const GLdouble *v) { set_current_tex_coords(v[0]); }
+void glTexCoord1fv(const GLfloat *v) { set_current_tex_coords(v[0]); }
+void glTexCoord1iv(const GLint *v) { set_current_tex_coords(v[0]); }
+void glTexCoord1sv(const GLshort *v) { set_current_tex_coords(v[0]); }
+
+void glTexCoord2dv(const GLdouble *v) { set_current_tex_coords(v[0], v[1]); }
+void glTexCoord2fv(const GLfloat *v) { set_current_tex_coords(v[0], v[1]); }
+void glTexCoord2iv(const GLint *v) { set_current_tex_coords(v[0], v[1]); }
+void glTexCoord2sv(const GLshort *v) { set_current_tex_coords(v[0], v[1]); }
+
+void glTexCoord3dv(const GLdouble *v) { set_current_tex_coords(v[0], v[1], v[2]); }
+void glTexCoord3fv(const GLfloat *v) { set_current_tex_coords(v[0], v[1], v[2]); }
+void glTexCoord3iv(const GLint *v) { set_current_tex_coords(v[0], v[1], v[2]); }
+void glTexCoord3sv(const GLshort *v) { set_current_tex_coords(v[0], v[1], v[2]); }
+
+void glTexCoord4dv(const GLdouble *v) { set_current_tex_coords(v[0], v[1], v[2], v[3]); }
+void glTexCoord4fv(const GLfloat *v) { set_current_tex_coords(v[0], v[1], v[2], v[3]); }
+void glTexCoord4iv(const GLint *v) { set_current_tex_coords(v[0], v[1], v[2], v[3]); }
+void glTexCoord4sv(const GLshort *v) { set_current_tex_coords(v[0], v[1], v[2], v[3]); }
